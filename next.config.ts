2f234b5beb951c1cb3459c39bd752516bb8f76fc
Tecnosalud Internacional <tecnolsalud@gmail.com>
2025-07-21 14:48:51 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    allowedDevOrigins: [
+      'https://6000-firebase-studio-1751988148835.cluster-joak5ukfbnbyqspg4tewa33d24.cloudworkstations.dev',
+    ],
+  },
 };
 
 export default nextConfig;
